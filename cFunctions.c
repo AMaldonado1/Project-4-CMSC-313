a972cf52e04431d1e6b640321773d8d8cbd12e13
JeremyJb134 <jjbboy@hotmail.com>
2023-12-06 22:26:23 -0500

    read

--- cFunctions.c
+++ cFunctions.c
@@ -5,3 +5,13 @@ void display(char *arr[]){
         printf("Message[%d]: %s\n", i, arr[i]);
     }
 }
+
+void read(char *arr[]){
+    int index = -1;
+    while(index < 0 || index > 9){
+        printf("What string do you want to replace? ");
+        scanf("%d", &index);
+        if(index < 0 || index > 9)
+            printf("Invalid String Location. \n");
+    }
+}
